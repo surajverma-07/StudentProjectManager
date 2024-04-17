a9544fbfa196f0c421adf35ad104ce0881dbbe9b
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2024-04-17 15:32:34 +0530

    Footer color and content modified

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import Logo from "../Logo";
 
 function Footer() {
   return (
-    <section className="relative overflow-hidden py-10 bg-gray-400 border border-t-2 border-t-black">
+    <section className="relative overflow-hidden py-4 bg-slate-800 text-slate-300  border-t-2 border-t-black">
       <div className="relative z-10 mx-auto max-w-7xl px-4">
         <div className="-m-6 flex flex-wrap">
           <div className="w-full p-6 md:w-1/2 lg:w-5/12">
@@ -13,127 +13,67 @@ function Footer() {
                 <Logo width="180px" />
               </div>
               <div>
-                <p className="text-sm text-gray-600">
+                <p className="text-sm text-slate-300">
                   &copy; Copyright 2024. All Rights Reserved by Coders United.
                 </p>
               </div>
             </div>
           </div>
-          <div className="w-full p-6 md:w-1/2 lg:w-2/12">
-            <div className="h-full">
-              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
-                Company
-              </h3>
-              <ul>
-                <li className="mb-4">
-                  <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                    to="/"
-                  >
-                    Features
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                    to="/"
-                  >
-                    Pricing
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                    to="/"
-                  >
-                    Affiliate Program
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                    to="/"
-                  >
-                    Press Kit
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="w-full p-6 md:w-1/2 lg:w-2/12">
-            <div className="h-full">
-              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
-                Support
-              </h3>
-              <ul>
-                <li className="mb-4">
-                  <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                    to="/"
-                  >
-                    Account
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                    to="/"
-                  >
-                    Help
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                    to="/"
-                  >
-                    Contact Us
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                    to="/"
-                  >
-                    Customer Support
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="w-full p-6 md:w-1/2 lg:w-3/12">
-            <div className="h-full">
-              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
-                Legals
-              </h3>
-              <ul>
-                <li className="mb-4">
-                  <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                    to="/"
-                  >
-                    Terms &amp; Conditions
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                    to="/"
-                  >
-                    Privacy Policy
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
-                    to="/"
-                  >
-                    Licensing
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
+         
+          <div className='  w-96 mt-10 ml-60'>
+                            <h2 className="mb-6 text-md  text-center  font-semibold text-[#FC6736]  uppercase">Follow us</h2>
+                            <div className='flex justify-evenly'>
+                               <div>
+                              <ul className="text-[#0C2D57] dark:text-[#EDEDED] font-medium">
+                                <li className="mb-4">
+                                    <a
+                                        href="https://github.com/surajverma-07"
+                                        className="hover:underline"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Github
+                                    </a>
+                                 </li>
+                                 <li>
+                                 <a
+                                        href="https://www.instagram.com/surajverma_00_7/"
+                                        className="hover:underline"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Instagram
+                                    </a>
+                                </li>
+                            </ul>
+                               </div>
+                               <div>
+                              <ul className="text-[#0C2D57] dark:text-[#EDEDED] font-medium">
+                                <li className="mb-4">
+                                    <a
+                                        href="https://www.linkedin.com/in/suraj-kumar-verma-699b5a245/"
+                                        className="hover:underline"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Linkdin
+                                    </a>
+                                 </li>
+                                 <li>
+                                 <a
+                                        href="https://leetcode.com/surajverma_00_7/"
+                                        className="hover:underline"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Leetcode
+                                    </a>
+                                </li>
+                            </ul>
+                               </div>
+                            </div>
+                        </div>
+       
         </div>
       </div>
     </section>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,27 +15,13 @@ function Home() {
     });
   }, []);
 
-  if (isUserAuthenticated) {
     return (
-      <div className="w-full py-3 text-center md:text-2xl text-white lg:text-4xl h-full min-h-screen bg-[url('../../src/assets/mainbg1.jpg')] bg-no-repeat bg-center bg-cover   ">
+      <div className="w-full py-3 text-center md:text-2xl text-slate-300  lg:text-5xl h-full min-h-screen bg-[url('../../src/assets/mainbg1.jpg')] bg-no-repeat bg-center bg-cover   ">
          <h2 className="maintext font-bold">Student Project Manager</h2>
       </div>
     );
-  } else {
-    return (
-      <div className="w-full py-8 mt-4 text-center">
-        <Container>
-          <div className="flex flex-wrap">
-            <div className="p-2 w-full">
-              <h1 className="text-2xl font-bold hover:text-gray-500 ">
-                Login To See Projects
-              </h1>
-            </div>
-          </div>
-        </Container>
-      </div>
-    );
+ 
   }
-}
+
 
 export default Home;
